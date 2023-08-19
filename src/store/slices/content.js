b9683dd8d445858e9a2e24cffdb224b0540945a2
maxmarscaramella <92364800+maxmarscaramella@users.noreply.github.com>
2023-08-19 22:57:15 +0200

    a new set of example data

--- src/store/slices/content.js
+++ src/store/slices/content.js
@@ -2,156 +2,300 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const initialState = {
     tree: [{
-        id: "simple-menu",
-        label: "Simple Menu",
+        id: "farm",
+        label: "A farm just out of the town",
         type: "menu",
         data: {
-            textContent: "This is a simple menu",
+            textContent: "The farm just out of the town",
             children: [
                 {
-                    id: "even-more-nested-menu",
-                    label: "Even more nested menu",
-                    type: "menu",
+                    id: "farm-description",
+                    label: "A description of the farm",
+                    type: "information",
                     data: {
-                        textContent: "This is a even more nested menu",
-                        children: [
-                            {
-                                id: "regular-description",
-                                label: "Regular Description",
-                                type: "information",
-                                data: {
-                                    textContent: "This is a regular description"
-                                }
-                            }
-                        ]
+                        textContent: "The farm is located between the city gates, the forest and the wizard's castle, which is located on the hill. The farm is run by an old farmer and his wife, who have a 10 year old son. The farm is surrounded by a stone wall, with a wooden gate. The house is a wooden construction with a thatched roof. The farm has a small vegetable garden, a chicken coop, a sheep pen and a pig pen. The farm also has a water well."
+                    }
+                },
+                {
+                    id: "events-farm",
+                    label: "The events around the farm",
+                    type: "table",
+                    data: {
+                        textContent: "Events around the farm",
+                        table: "farm-events"
                     },
                 },
                 {
-                    id: "inner-text-content",
-                    label: "Simple Inner Text Content",
-                    type: "information",
-                    data: {
-                        textContent: "Please set some meaningful content data, this is just a default stub"
-                    }
-                },
-                {
-                    id: "value-based-inner-table",
-                    label: "Value based inner table",
+                    id: "npc-farm",
+                    label: "The NPCs encountered at the farm",
                     type: "table",
                     data: {
-                        textContent: "This is a value based table",
-                        prefix: "This is a prefix",
-                        table: "weathersummer weathersummer"
-                    }
-                }
+                        textContent: "NPC near the farm",
+                        table: "farm-npc"
+                    },
+                },
+                {
+                    id: "weather-farm",
+                    label: "The weather around the farm",
+                    type: "table",
+                    data: {
+                        textContent: "Typical farm weather conditions",
+                        table: "farm-weather"
+                    },
+                },
             ]
-        }
-    }, {
-        id: "text-content",
-        label: "Simple Text Content",
-        type: "information",
-        data: {
-            textContent: "Please set some meaningful content data, this is just a default stub"
         }
     },
     {
-        id: "value-based-table",
-        label: "Value based table",
-        type: "table",
+        id: "explanation",
+        label: "Explanation of the example tables",
+        type: "information",
         data: {
-            textContent: "This is a value based table",
-            prefix: "This is a prefix",
-            table: "weathersummer weathersummer"
+            textContent: "The data herein is only an example. The other item in this menu is an example of a sub-menu. Inside of it there are three tables: the farm weather is a table of values only; farm NPCs is a table that has some values and others are taken from two other tables (NPC from Castle and NPC from Town). The last table is that of the farm events: in it there are values, references to the farm animals table and references to the farm NPC table which, as seen before, in turn contains references to other tables.",
         }
-    }],
+    }
+    ],
     tables: [
         {
-            id: "weathersummer",
-            description: "Weather, summer",
-            tags: ["WEATHER", "SUMMER"],
+            id: "farm-events",
+            description: "Farm Events",
+            tags: ["events", "farm"],
             rng: [
                 {
                     min: 1,
-                    max: 1,
-                    result: "Incredibly cold cold.",
+                    max: 2,
+                    result: "A sound of heavy footsteps in the woods",
                 },
                 {
-                    min: 2,
+                    min: 3,
                     max: 4,
-                    table: "weatherwinter",
+                    result: "A group of Goblins approaching",
                 },
                 {
                     min: 5,
                     max: 8,
-                    prefix: "Looks like winter..",
-                    table: "weatherwinter",
-                    postfix: " (maybe it's just a cold summer)",
+                    prefix: "NPC encounter: ",
+                    table: "farm-npc",
                 },
                 {
                     min: 9,
                     max: 11,
-                    result: "Fuzzy Warm (not really)",
+                    prefix: "You meet an animal: ",
+                    table: "farm-animals",
                 },
                 {
                     min: 12,
+                    max: 12,
+                    prefix: "Weather is changing: ",
+                    table: "farm-weather",
+                },
+                {
+                    min: 13,
+                    max: 15,
+                    result: "A hum of insects coming from the woods",
+                },
+                {
+                    min: 16,
+                    max: 17,
+                    result: "A whirlwind that kicks up the dust",
+                },
+                {
+                    min: 18,
                     max: 18,
-                    prefix: "And now, for something different..",
-                    table: "weatherwinter weathersummer weatherwinter",
-                    postfix: " (I don't know what's going on)",
+                    result: "A dull thud coming from the woods",
                 },
                 {
                     min: 19,
                     max: 20,
-                    result: "Hot in the city",
+                    prefix: "Weather is changing: ",
+                    table: "farm-weather",
                 },
             ]
         },
         {
-            id: "weatherwinter",
-            description: "Weather, winter",
-            tags: ["WEATHER", "COLD"],
+            id: "farm-weather",
+            description: "Weather around the farm",
+            tags: ["weather", "farm"],
             rng: [
                 {
                     min: 1,
                     max: 1,
-                    prefix: "It's: ",
-                    result: "BRRR.. Cold.",
-                    postfix: "!",
+                    result: "Cold and foggy",
                 },
                 {
                     min: 2,
                     max: 4,
-                    prefix: "It's: ",
-                    result: "SO Cold",
-                    postfix: "!",
+                    result: "Cool",
                 },
                 {
                     min: 5,
                     max: 8,
-                    prefix: "It's: ",
-                    result: "This cold's not fair",
-                    postfix: "!",
+                    result: "Wind and rain",
                 },
                 {
                     min: 9,
                     max: 11,
-                    prefix: "It's: ",
-                    result: "Cold as ice",
-                    postfix: "!",
+                    result: "Warm and sunny",
                 },
                 {
                     min: 12,
                     max: 18,
-                    prefix: "It's: ",
-                    result: "Arguably cold",
-                    postfix: "!",
+                    result: "Temperate and variable",
                 },
                 {
                     min: 19,
                     max: 20,
-                    prefix: "It's: ",
-                    result: "Not warm at all",
-                    postfix: "!",
+                    result: "Unbearable heat",
+                },
+            ]
+        },
+        {
+            id: "farm-animals",
+            description: "Animals around the farm",
+            tags: ["animals", "farm"],
+            rng: [
+                {
+                    min: 1,
+                    max: 1,
+                    result: "A lone wolf, out of the woods",
+                },
+                {
+                    min: 2,
+                    max: 4,
+                    result: "A stray cat purring around your legs",
+                },
+                {
+                    min: 5,
+                    max: 8,
+                    result: "One of the farm dogs, barking at you",
+                },
+                {
+                    min: 9,
+                    max: 11,
+                    result: "A group of sheep, looking at you curiously",
+                },
+                {
+                    min: 12,
+                    max: 18,
+                    result: "Hens squawking and chasing each other",
+                },
+                {
+                    min: 19,
+                    max: 20,
+                    result: "Castle hunting dogs, chasing a rabbit",
+                },
+            ]
+        },
+        {
+            id: "farm-npc",
+            description: "NPCs around the farm",
+            tags: ["npc", "farm"],
+            rng: [
+                {
+                    min: 1,
+                    max: 1,
+                    result: "Marius the farmer, leaning on his shovel",
+                },
+                {
+                    min: 2,
+                    max: 4,
+                    prefix: "From the castle.. ",
+                    table: "castle-npc",
+                },
+                {
+                    min: 5,
+                    max: 8,
+                    result: "Marius the farmer, harvesting potatoes",
+                },
+                {
+                    min: 9,
+                    max: 11,
+                    result: "Evelina the peasant woman who cleans the farmyard",
+                },
+                {
+                    min: 12,
+                    max: 16,
+                    result: "Raimondo the little rascal, who makes faces",
+                },
+                {
+                    min: 17,
+                    max: 20,
+                    prefix: "From the city.. ",
+                    table: "city-npc",
+                },
+            ]
+        },        
+        {
+            id: "castle-npc",
+            description: "NPCs around the castle",
+            tags: ["npc", "castle"],
+            rng: [
+                {
+                    min: 1,
+                    max: 1,
+                    result: "The wizard, who looks at you curiously",
+                },
+                {
+                    min: 2,
+                    max: 4,
+                    result: "Battista the butler, who looks at you suspiciously",
+                },
+                {
+                    min: 5,
+                    max: 8,
+                    result: "Maria the cook, who looks at you distractedly",
+                },
+                {
+                    min: 9,
+                    max: 11,
+                    result: "The castle gamekeeper",
+                },
+                {
+                    min: 12,
+                    max: 18,
+                    result: "Two students of magic, disciples of the castle wizard",
+                },
+                {
+                    min: 19,
+                    max: 20,
+                    result: "The wizard of the castle looking out of breath",
+                },
+            ]
+        },    
+        {
+            id: "city-npc",
+            description: "NPCs around the city",
+            tags: ["npc", "city"],
+            rng: [
+                {
+                    min: 1,
+                    max: 1,
+                    result: "The mayor's wife",
+                },
+                {
+                    min: 2,
+                    max: 4,
+                    result: "The mayor",
+                },
+                {
+                    min: 5,
+                    max: 8,
+                    result: "A market trader",
+                },
+                {
+                    min: 9,
+                    max: 11,
+                    result: "Two city guards",
+                },
+                {
+                    min: 12,
+                    max: 18,
+                    result: "A citizen who looks at you with curiosity",
+                },
+                {
+                    min: 19,
+                    max: 20,
+                    result: "A schoolgirl from the city",
                 },
             ]
         },
