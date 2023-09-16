e891ae1683d21d5d55c01202fe6055bde6049994
maxmarscaramella <92364800+maxmarscaramella@users.noreply.github.com>
2023-09-16 20:01:23 +0200

    1.4.0 Added CKEditor for text content editing.

--- src/components/authoring/MenuEdit.js
+++ src/components/authoring/MenuEdit.js
@@ -304,18 +304,18 @@ const MenuEdit = (props) => {
                                     config= {{language: {ui: navigator.language.substring(0, 2), content: navigator.language.substring(0, 2) }}}
                                     onReady={editor => {
                                         // You can store the "editor" and use when it is needed.
-                                        console.log('Editor is ready to use!', editor);
+                                        //console.log('Editor is ready to use!', editor);
                                     }}
                                     onChange={(event, editor) => {
                                         const data = editor.getData();
-                                        console.log({ event, editor, data });
+                                        //console.log({ event, editor, data });
                                         setCurrentMenuContent(data)
                                     }}
                                     onBlur={(event, editor) => {
-                                        console.log('Blur.', editor);
+                                        //console.log('Blur.', editor);
                                     }}
                                     onFocus={(event, editor) => {
-                                        console.log('Focus.', editor);
+                                        //console.log('Focus.', editor);
                                     }}
                                 />
                                 :
