9e397c6d816dddd30e318d378accde40219e08b7
maxmarscaramella <92364800+maxmarscaramella@users.noreply.github.com>
2023-09-16 22:34:19 +0200

    1.4.0 Added CKEditor for text content editing.

--- src/components/authoring/MenuEdit.js
+++ src/components/authoring/MenuEdit.js
@@ -25,9 +25,7 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
 import './style.css'
-
 
 
 const MenuEdit = (props) => {
