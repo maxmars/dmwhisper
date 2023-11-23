5ff6b4bf65165d338ff7e40ff1a46efc671a8067
Massimiliano Marsiglietti <maxmars@hotmail.it>
2023-11-23 16:06:27 +0100

    Added cordova stuff so you can run it as a real app

---
+++ cordova/android_build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export ANDROID_SDK_ROOT=/home/maxmars/Android/Sdk
+export PATH=/home/maxmars/Android/Sdk/build-tools/33.0.2/:/home/maxmars/Android/Sdk/platform-tools:/home/maxmars/Android/Sdk/tools:/home/maxmars/Android/gradle-7.3.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:/home/maxmars/.dotnet/tools:/home/maxmars/bin:/opt/mssql-tools/bin:/usr/lib/jvm/java-11-openjdk-amd64/bin:/home/maxmars/.dotnet/tools:/home/maxmars/bin:/opt/mssql-tools/bin:/usr/lib/jvm/java-11-openjdk-amd64/bin:/opt/mssql-tools/bin:/usr/lib/jvm/java-11-openjdk-amd64/bin
+export ORG_GRADLE_PROJECT_cdvBuildToolsVersion=33.0.2
+cordova build android --debug
+rm -f release/*.apk
+cp platforms/android/app/build/outputs/apk/debug/app-debug.apk release/dmwhisper.apk
