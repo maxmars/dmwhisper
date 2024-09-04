6e0da777f851d6cc300ca44059009261dd829453
Massimiliano Marsiglietti <maxmars@hotmail.it>
2024-09-04 16:20:27 +0200

    WIP random dungeons

--- src/components/testbed/Test.js
+++ src/components/testbed/Test.js
@@ -1,9 +1,24 @@
 import React, { useRef, useEffect, useState } from 'react';
-import DungeonCreate, { Dungeon, DungeonCreateTest } from '../../snippets/Dungeon.js';
-import { createBox } from '@mui/system';
+import { Dungeon } from '../../snippets/Dungeon.js';
 
 
 const Test = () => {
+
+  const DungeonCreate = () => {
+    const props = {
+      width: 20,
+      height: 20,
+      roomTypes: [{ name: 'kitchen', occurrences: 1 }, { name: 'bedroom', occurrences: 3 }, { name: 'dining room', occurrences: 1 }, { name: 'bathroom', occurrences: 2 }, { name: 'study', occurrences: 2 }],
+      roomMinSize: 3,
+      roomMaxSize: 4
+    };
+
+    const dungeon = new Dungeon(props.width, props.height, props.roomTypes);
+    dungeon.generateRooms(props.roomMinSize, props.roomMaxSize);
+    //dungeon.generateCorridors();
+    return dungeon;
+  }
+
   const canvasRef = useRef(null);
   const mouseDownRef = useRef(false);
   const zoomlevel = useState(1);
@@ -22,7 +37,14 @@ const Test = () => {
     height: window.innerHeight,
   });
 
-  const [dungeon, setDungeon] = useState(DungeonCreateTest());
+  const [dungeon, setDungeon] = useState(DungeonCreate());
+  const [drawnCorridors, setDrawnCorridors] = useState([]);
+
+  const [maxCoordinateX, setMaxCoordinateX] = useState(0);
+  const [maxCoordinateY, setMaxCoordinateY] = useState(0);
+  const [xInc, setXInc] = useState(0);
+  const [yInc, setYInc] = useState(0);
+
 
   useEffect(() => {
     const handleResize = () => {
@@ -40,6 +62,33 @@ const Test = () => {
     };
   }, []);
 
+  useEffect(() => {
+    let maxCoordinateX = 0;
+    dungeon.rooms.forEach((room) => {
+      const roomMaxX = room.x + room.width;
+      if (roomMaxX > maxCoordinateX) {
+        maxCoordinateX = roomMaxX;
+      }
+    });
+
+    let maxCoordinateY = 0;
+    dungeon.rooms.forEach((room) => {
+      const roomMaxY = room.y + room.height;
+      if (roomMaxY > maxCoordinateY) {
+        maxCoordinateY = roomMaxY;
+      }
+    });
+
+    const xInc = windowSize.width / maxCoordinateX;
+    const yInc = (windowSize.height - 170) / maxCoordinateY;
+
+    setMaxCoordinateX(maxCoordinateX);
+    setMaxCoordinateY(maxCoordinateY);
+    setXInc(xInc);
+    setYInc(yInc);
+  }, [dungeon, windowSize, dungeon.rooms]);
+
+
   // Ri-disegniamo la mappa quando qualcosa cambia
   useEffect(() => {
     if (canvasRef && canvasRef.current) {
@@ -53,6 +102,35 @@ const Test = () => {
     windowSize.width,
     windowSize.height
   ]);
+
+  const drawAllCorridors = (color) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    // draw the corridors
+    context.strokeStyle = color;
+    context.lineWidth = 5;
+
+    for (let i = 1; i < dungeon.rooms.length; i++) {
+      // Se i è presente nell'array drawnCorridors, allora disegna il corridoio
+      if (drawnCorridors.includes(i)) {
+        let prevRoom = dungeon.rooms[i - 1];
+        let currRoom = dungeon.rooms[i];
+
+        let prevCenter = { x: prevRoom.x + Math.floor(prevRoom.width / 2), y: prevRoom.y + Math.floor(prevRoom.height / 2) };
+        let currCenter = { x: currRoom.x + Math.floor(currRoom.width / 2), y: currRoom.y + Math.floor(currRoom.height / 2) };
+
+        if (i % 2 === 0) {
+          context.strokeRect(prevCenter.x * xInc, prevCenter.y * yInc, currCenter.x * xInc - prevCenter.x * xInc, 5);
+          context.strokeRect(currCenter.x * xInc, prevCenter.y * yInc, 5, currCenter.y * yInc - prevCenter.y * yInc);
+        } else {
+          context.strokeRect(prevCenter.x * xInc, prevCenter.y * yInc, 5, currCenter.y * yInc - prevCenter.y * yInc);
+          context.strokeRect(prevCenter.x * xInc, currCenter.y * yInc, currCenter.x * xInc - prevCenter.x * xInc, 5);
+        }
+      }
+    }
+
+  }
 
   const drawMap = () => {
     const canvas = canvasRef.current;
@@ -70,25 +148,6 @@ const Test = () => {
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    let maxCoordinateX = 0;
-    dungeon.rooms.forEach((room) => {
-      const roomMaxX = room.x + room.width;
-      if (roomMaxX > maxCoordinateX) {
-        maxCoordinateX = roomMaxX;
-      }
-    });
-
-    let maxCoordinateY = 0;
-    dungeon.rooms.forEach((room) => {
-      const roomMaxY = room.y + room.height;
-      if (roomMaxY > maxCoordinateY) {
-        maxCoordinateY = roomMaxY;
-      }
-    });
-
-    const xInc = bcr.width / maxCoordinateX;
-    const yInc = bcr.height / maxCoordinateY;
-
     // draw the dungeon rooms
     context.fillStyle = 'green';
     context.lineWidth = 5;
@@ -97,32 +156,58 @@ const Test = () => {
       context.fillRect(room.x * xInc, room.y * yInc, room.width * xInc, room.height * yInc);
     });
 
-    // draw the corridors
-    context.strokeStyle = 'blue';
-    context.lineWidth = 5;
+    drawAllCorridors('red');
 
-    for (let i = 1; i < dungeon.rooms.length; i++) {
-      let prevRoom = dungeon.rooms[i - 1];
-      let currRoom = dungeon.rooms[i];
-
-      let prevCenter = { x: prevRoom.x + Math.floor(prevRoom.width / 2), y: prevRoom.y + Math.floor(prevRoom.height / 2) };
-      let currCenter = { x: currRoom.x + Math.floor(currRoom.width / 2), y: currRoom.y + Math.floor(currRoom.height / 2) };
-
-      if (i % 2 === 0) {
-        context.strokeRect(prevCenter.x * xInc, prevCenter.y * yInc, currCenter.x * xInc - prevCenter.x * xInc, 5);
-        context.strokeRect(currCenter.x * xInc, prevCenter.y * yInc, 5, currCenter.y * yInc - prevCenter.y * yInc);
-      } else {
-        context.strokeRect(prevCenter.x * xInc, prevCenter.y * yInc, 5, currCenter.y * yInc - prevCenter.y * yInc);
-        context.strokeRect(prevCenter.x * xInc, currCenter.y * yInc, currCenter.x * xInc - prevCenter.x * xInc, 5);
-      }
-    }
-
+    // draw borders
     context.strokeStyle = 'yellow';
     context.lineWidth = 5;
     context.strokeRect(0, 0, canvas.width, canvas.height);
 
     //console.log(bcr);
   }
+
+  const getClickedRoomNumber = (x, y) => {
+    for (let i = 0; i < dungeon.rooms.length; i++) {
+      const room = dungeon.rooms[i];
+      if (x >= room.x && x <= room.x + room.width && y >= room.y && y <= room.y + room.height) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  const roomClicked = (x, y) => {
+    let clickedRoomNumber = getClickedRoomNumber(x, y);
+    //console.log(clickedRoomNumber);
+
+    if (clickedRoomNumber === 0) {
+      clickedRoomNumber = 1;
+    }
+
+    if (drawnCorridors.includes(clickedRoomNumber)) {
+
+      let tempDrawnCorridors = [...drawnCorridors];
+
+      tempDrawnCorridors = tempDrawnCorridors.filter((roomNumber) => roomNumber !== clickedRoomNumber);
+
+      if (clickedRoomNumber < dungeon.rooms.length - 1 && drawnCorridors.includes(clickedRoomNumber + 1)) {
+        tempDrawnCorridors = tempDrawnCorridors.filter((roomNumber) => roomNumber !== clickedRoomNumber + 1);
+      }
+
+      setDrawnCorridors(tempDrawnCorridors);
+
+    } else {
+      let tempDrawnCorridors = [...drawnCorridors, clickedRoomNumber];
+
+      if (clickedRoomNumber < dungeon.rooms.length - 1 && !drawnCorridors.includes(clickedRoomNumber + 1)) {
+        tempDrawnCorridors = [...tempDrawnCorridors, clickedRoomNumber + 1];
+      }
+
+      setDrawnCorridors(tempDrawnCorridors);
+    }
+
+  }
+
 
   return (
     <canvas
@@ -136,6 +221,30 @@ const Test = () => {
       onMouseOut={(e) => {
         setCurrentMousePosition(null);
         //mouseUp(e);
+        return;
+      }}
+      onClick={(e) => {
+        //mouseClick(e);
+        // get the canvas coordinates of the click
+        const canvas = canvasRef.current;
+        const bcr = canvas.getBoundingClientRect();
+        const x = (e.clientX - bcr.left) / xInc;
+        const y = (e.clientY - bcr.top) / yInc;
+
+        roomClicked(x, y);
+
+        return;
+      }}
+      onTouchStart={(e) => {
+        // get the canvas coordinates of the touch
+        const canvas = canvasRef.current;
+        const bcr = canvas.getBoundingClientRect();
+        const touch = e.touches[0];
+        const x = (touch.clientX - bcr.left) / xInc;
+        const y = (touch.clientY - bcr.top) / yInc;
+    
+        roomClicked(x, y);
+    
         return;
       }}
       id="canvas"
